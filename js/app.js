4273a96a08722d359a1543b2259251bd44c27f09
Cesar-Acevedo23 <ces.acevedo.23@gmail.com>
2021-07-11 15:45:10 -0400

    Agregando validaciones

--- js/app.js
+++ js/app.js
@@ -1,10 +1,20 @@
 //Variables
 const btnEnviar = document.querySelector("#enviar")
 
+//Variables para campos
+const email = document.querySelector("#email");
+const asunto = document.querySelector("#asunto");
+const mensaje = document.querySelector("#mensaje");
+
 //Listener que escucha cuando carga la pagina
 evenListeners();
 function evenListeners(){
+    //Cuando la app arranca
     document.addEventListener("DOMContentLoaded", iniciarApp);
+    // Campos del formulario
+    email.addEventListener("blur", validarFormulario);
+    asunto.addEventListener("blur", validarFormulario);
+    mensaje.addEventListener("blur", validarFormulario);
 }
 
 //Funciones
@@ -14,3 +24,11 @@ function iniciarApp(){
     btnEnviar.classList.add("cursor-not-allowed", "opacity-50")//clsaes de tailwind
 }
 
+function validarFormulario(e){
+    if(e.target.value.length > 0){
+        console.log("si hay algo");
+    }
+    else{
+        e.target.classList.add("border", "border-red-500") //clases de tailwind
+    }
+}
